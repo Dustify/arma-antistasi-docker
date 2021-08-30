6a1c848a718f600630c2ba1d0f73a501e6f03371
Adrian Johnson <aj@dustify.com>
2021-08-30 19:17:15 +0100

    added install dir to run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ while true
 do
         cd ~/steam
 
-        ./steamcmd.sh +login username +app_update 233780 +quit
+        ./steamcmd.sh +login USERNAME PASSWORD +force_install_dir ~/arma +app_update 233780 +quit
 
         cd ~/arma
         ./arma3server -config=server.cfg -profiles -port 2302
