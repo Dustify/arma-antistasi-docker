9bd9f081c9f7fd049130591ffe352b72861f973c
Adrian Johnson <aj@dustify.com>
2021-09-05 01:37:10 +0100

    fixed yet another path

--- run-hc.sh
+++ run-hc.sh
@@ -6,6 +6,6 @@ do
 
         ./steamcmd.sh +login USERNAME PASSWORD +force_install_dir ~/arma +app_update 233780 +quit
 
-        cd ~/Steam/steamapps/common/Arma\ 3\ Server
+        cd ~/arma
         ./arma3server -client -connect=127.0.0.1 -port=2302 -enableHT -noLogs;
 done
